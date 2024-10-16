88c3d7d94b549c9e5ec60124339802f4c5eb5e37
Vinil Reddy <igris1305vr@gmail.com>
2024-10-16 14:33:17 -0400

    dynamic pages 1

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Registration from "./components/Register/Register"
+import Dealers from './components/Dealers/Dealers';
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Registration />} />
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
