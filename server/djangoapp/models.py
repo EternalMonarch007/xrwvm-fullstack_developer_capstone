45b4534533b849b6a6a00e456abdcb46e5ab5897
Vinil Reddy <130872535+EternalMonarch007@users.noreply.github.com>
2024-10-17 00:11:13 +0000

    workflow2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,9 +4,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
-
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -37,4 +34,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-        
