b21e44b9e80ba5e6bdd374adea6b01396a9d512e
Vinil Reddy <130872535+EternalMonarch007@users.noreply.github.com>
2024-10-17 01:36:19 +0000

    python workflow 5 test 1

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,7 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
       car_make_instances.append(CarMake.objects.create(name=data['name'],
-       description=data['description']))
+                                                       description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -25,7 +25,7 @@ def initiate():
        "car_make": car_make_instances[0]},
       {"name": "A-Class", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[1]},
-      {"name": "C-Class", "type": "SUV", "year": 2023, 
+      {"name": "C-Class", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[1]},
       {"name": "E-Class", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[1]},
@@ -51,4 +51,4 @@ def initiate():
 
     for data in car_model_data:
       CarModel.objects.create(name=data['name'], car_make=data['car_make'],
-       type=data['type'], year=data['year'])
+                               type=data['type'], year=data['year'])
