746e5e4ed49facae11fa2ff91b9ba5e92670d7ea
Vinil Reddy <130872535+EternalMonarch007@users.noreply.github.com>
2024-10-17 01:26:38 +0000

    python workflow 4 test 5

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
  'https://vinilreddy13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://vinilreddy13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+                         'https://vinilreddy13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -95,7 +96,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_
+         validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
